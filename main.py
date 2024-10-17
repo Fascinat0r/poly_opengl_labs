a8334116fbbf0a7ac36e1fe23518a58591809258
Fascinat0r <fascinator2904@gmail.com>
2024-10-18 00:28:02 +0300

    Add Scene class

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from OpenGL.GLUT import *
 
 from camera import Camera
 from handlers import key_pressed, key_released, handle_camera_movement, mouse_movement
-from scene import draw_axes, draw_grid
+from scene import Scene  # Класс сцены
 
 # Создаем камеру
 camera = Camera([0.0, 0.0, 5.0], [0.0, 1.0, 0.0], -90.0, 0.0)
 
+# Создаем сцену
+scene = Scene()
 
 def init():
-    glEnable(GL_DEPTH_TEST)
-    glClearColor(0.0, 0.0, 0.0, 1.0)
+    glEnable(GL_DEPTH_TEST)  # Включаем тест глубины для 3D
+    glClearColor(0.0, 0.0, 0.0, 1.0)  # Черный фон
 
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(45, 800 / 600, 0.1, 100.0)
+    gluPerspective(45, 800 / 600, 0.1, 100.0)  # Настройка перспективы
     glMatrixMode(GL_MODELVIEW)
-
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -26,17 +27,12 @@ def display():
 
     # Получаем матрицу вида камеры
     position, target, up = camera.get_view_matrix()
+    gluLookAt(position[0], position[1], position[2], target[0], target[1], target[2], up[0], up[1], up[2])
 
-    # Исправленный вызов gluLookAt с полным набором аргументов
-    gluLookAt(position[0], position[1], position[2],
-              target[0], target[1], target[2],
-              up[0], up[1], up[2])
-
-    draw_axes()
-    draw_grid()
+    # Отрисовка сцены
+    scene.render()
 
     glutSwapBuffers()
-
 
 def update(value):
     """Обновление состояния сцены."""
@@ -44,24 +40,20 @@ def update(value):
     glutPostRedisplay()
     glutTimerFunc(16, update, 0)
 
-
 def key_press_wrapper(key, x, y):
     key_pressed(key, x, y)
-
 
 def key_release_wrapper(key, x, y):
     key_released(key, x, y)
 
-
 def mouse_motion_wrapper(x, y):
     mouse_movement(x, y, camera)
-
 
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(800, 600)
-    glutCreateWindow(b"3D Scene with Camera")
+    glutCreateWindow(b"3D Scene with Cube")
 
     init()
 
@@ -79,7 +71,6 @@ def main():
     glutTimerFunc(16, update, 0)
 
     glutMainLoop()
-
 
 if __name__ == "__main__":
     main()
--- scene.py
+++ scene.py
@@ -1,33 +1,57 @@
 from OpenGL.GL import *
+from shapes.cube import Cube
 
+class Scene:
+    def __init__(self):
+        # Здесь будут храниться объекты, добавленные в сцену
+        self.objects = []
+        # Добавим куб для примера
+        self.cube = Cube(position=[0.0, 0.0, 0.0], scale=1.0, color=[1.0, 0.0, 0.0])
 
-def draw_axes():
-    glBegin(GL_LINES)
+    def add_object(self, obj):
+        """Добавляем объекты (кубы, сферы и т.д.) в сцену."""
+        self.objects.append(obj)
 
-    # Оси
-    glColor3f(1, 0, 0)  # Ось X
-    glVertex3f(-10.0, 0.0, 0.0)
-    glVertex3f(10.0, 0.0, 0.0)
+    def draw_axes(self):
+        """Отрисовка осей координат."""
+        glBegin(GL_LINES)
 
-    glColor3f(0, 1, 0)  # Ось Y
-    glVertex3f(0.0, -10.0, 0.0)
-    glVertex3f(0.0, 10.0, 0.0)
+        # Оси
+        glColor3f(1, 0, 0)  # Ось X
+        glVertex3f(-10.0, 0.0, 0.0)
+        glVertex3f(10.0, 0.0, 0.0)
 
-    glColor3f(0, 0, 1)  # Ось Z
-    glVertex3f(0.0, 0.0, -10.0)
-    glVertex3f(0.0, 0.0, 10.0)
+        glColor3f(0, 1, 0)  # Ось Y
+        glVertex3f(0.0, -10.0, 0.0)
+        glVertex3f(0.0, 10.0, 0.0)
 
-    glEnd()
+        glColor3f(0, 0, 1)  # Ось Z
+        glVertex3f(0.0, 0.0, -10.0)
+        glVertex3f(0.0, 0.0, 10.0)
 
+        glEnd()
 
-def draw_grid():
-    glColor3f(0.5, 0.5, 0.5)
-    glBegin(GL_LINES)
+    def draw_grid(self):
+        """Отрисовка сетки."""
+        glColor3f(0.5, 0.5, 0.5)
+        glBegin(GL_LINES)
 
-    for i in range(-10, 11):
-        glVertex3f(i, 0, -10)
-        glVertex3f(i, 0, 10)
-        glVertex3f(-10, 0, i)
-        glVertex3f(10, 0, i)
+        for i in range(-10, 11):
+            glVertex3f(i, 0, -10)
+            glVertex3f(i, 0, 10)
+            glVertex3f(-10, 0, i)
+            glVertex3f(10, 0, i)
 
-    glEnd()
+        glEnd()
+
+    def render(self):
+        """Основной метод для отрисовки всей сцены."""
+        self.draw_axes()
+        self.draw_grid()
+
+        # Отрисовка объектов в сцене (например, куба)
+        self.cube.draw()        # Отрисовка куба
+        self.cube.draw_edges()  # Отрисовка рёбер куба
+        for obj in self.objects:
+            obj.draw()
+            obj.draw_edges()
