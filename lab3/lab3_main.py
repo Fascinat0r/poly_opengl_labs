aa62f072e369b1ec3b9362ff7e8011a35987e8f3
Fascinat0r <fascinator2904@gmail.com>
2024-11-15 22:17:27 +0300

    Add a floor

--- lab3/lab3_main.py
+++ lab3/lab3_main.py
@@ -24,6 +24,9 @@ def main():
     texture = Texture("../data/textures/leafs.png")
     texture.load()
 
+    texture_cobblestone = Texture("../data/textures/grassy_cobblestone.jpg")
+    texture_cobblestone.load()
+
     # Источник света - точечный свет
     point_light = PointLight(position=[0.0, 5.0, 5.0],
                              ambient=[0.05, 0.05, 0.05],
@@ -38,12 +41,12 @@ def main():
         diffuse=[0.5, 0.5, 0.5],
         specular=[0.2, 0.2, 0.2],
         shininess=10.0,
-        texture=None,
+        texture=texture_cobblestone.texture_id,
         transparent=False
     )
 
     # Создаем объект плоскости
-    floor = Plane(position=[0.0, 0.0, 0.0], scale=10.0, rotation=[0.0, 0.0, 0.0], material=floor_material)
+    floor = Plane(position=[0.0, 0.0, 0.0], scale=40.0, rotation=[0.0, 0.0, 0.0], material=floor_material)
     scene.add_object(floor)
 
     # Создаем текстурированный куб
@@ -58,19 +61,6 @@ def main():
     textured_cube = Cube(position=[1.0, 1.0, -5.0], scale=2.0, rotation=[0.0, 0.0, 0.0],
                          material=textured_cube_material)
     scene.add_object(textured_cube)
-
-    # Создаем матовый куб
-    cube_material = Material(
-        ambient=[0.1, 0.1, 0.1],
-        diffuse=[0.8, 0.8, 0.8],
-        specular=[0.5, 0.5, 0.5],
-        shininess=32.0,
-        texture=None,
-        transparent=False
-    )
-
-    cube = Cube(position=[-1.0, 1.0, -5.0], scale=2.0, rotation=[0.0, 0.0, 0.0], material=cube_material)
-    scene.add_object(cube)
 
     # Создаем анимацию вращения источника света
     light_rotation_animation = LightRotationAnimation(point_light, radius=5.0, speed=30.0)
